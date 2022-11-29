1bc26794cf84b23a1d57d420fdb1173b21d47ec7
Your Name <you@example.com>
2022-11-29 22:45:57 +0530

    any message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Routes>
+        <Route exact path='/' element={<Login />}></Route>
+        <Route exact path='/login' element={<Login />}></Route>
+        <Route exact path='/signup' element={<Signup />}></Route>
+      </Routes>
     </div>
+  </Router>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Common.css';
+import socialImg from '../assets/images/social.PNG';
+import socialImgWithTitle from '../assets/images/social-with-title.PNG';
+import socialOnlyTitle from '../assets/images/social-only-title.PNG';
+
+const Login = () => {
+    return (
+        <div className='center-container'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-md-7 col-sm-12 d-flex flex-column justify-content-center align-items-center'>
+                        {/*<div className='mb-4'>
+                            <i className="fa-brands fa-instagram display-3"></i>
+                        </div>
+                        <div className='title-style'>
+                            Reactogram
+                        </div>
+                        <div>
+                            <img src={socialImg} />
+                        </div>*/}
+                        <img className='socialImgWithTitle' src={socialImgWithTitle} />
+                        <img className='socialOnlyTitle' src={socialOnlyTitle} />
+                    </div>
+                    <div className='col-md-5 col-sm-12'>
+                        <div class="card shadow shadow-md">
+                            <div class="card-body px-5">
+                                <h4 className='text-center my-4 fw-bold'>Log In</h4>
+                                <form>
+                                    <input className='mb-2 p-2 form-control input-color' type='text' placeholder='Phone number, username or email' />
+                                    <input className='p-2 form-control input-color' type='password' placeholder='Password' />
+                                    <div className='mt-3 d-grid'>
+                                        <button className='custom-btn custom-btn-blue'>Log In</button>
+                                    </div>
+                                    <div className='my-3'>
+                                        <span>
+                                            <hr className='text-muted dash-line d-inline-block' />
+                                            <span className='text-top'>OR</span>
+                                            <hr className='text-muted dash-line d-inline-block' />
+                                        </span>
+                                    </div>
+                                    <div className='mt-3 mb-5 d-grid'>
+                                        <button className='custom-btn custom-btn-white'>
+                                            <span className='text-muted' style={{fontSize: '13px'}}>Don't have an account?</span>
+                                            <span className='ms-1 text-info' style={{fontSize: '14px', fontWeight: '500'}}>Sign Up</span>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login
